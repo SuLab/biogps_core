84c6bf01f0e3ccea675585ea24c598e8a9f2f3d6
newgene <anewgene@yahoo.com>
2012-11-05 12:15:19 -0800

    using new mapping type keywords; adding "rebuild_dataset" convenient function.

--- src/biogps/apps/search/build_index_dataset.py
+++ src/biogps/apps/search/build_index_dataset.py
@@ -12,11 +12,11 @@ class BiogpsDatasetIndexer(BiogpsModelESIndexer):
         super(BiogpsModelESIndexer, self).__init__()
 
     def get_field_mapping(self):
-        m = self._get_field_mapping(extra_attrs={'id': ['geo_gds_id', 'geo_gpl_id', 'geo_gse_id',
-                                                        'platform_id', 'species'],
-                                                 'text': ['name', 'summary'],
-                                                 "float": ['popularity'],
-                                                 'boolean': ['default'],
+        m = self._get_field_mapping(extra_attrs={'id_type': ['geo_gds_id', 'geo_gpl_id', 'geo_gse_id',
+                                                             'platform_id', 'species'],
+                                                 'text_type': ['name', 'summary'],
+                                                 "float_type": ['popularity'],
+                                                 'boolean_type': ['default'],
                                                  'disabled_object': ['factors'],
                                                  'disabled_string': ['permission_style', 'display_params'],
                                                  'disabled_integer': ['platform_id', 'pubmed_id']
@@ -69,3 +69,11 @@ class BiogpsDatasetReporterIndexer(BiogpsESIndexerBase):
         print conn.flush()
         print conn.refresh()
         print 'Done! - {} docs indexed.'.format(cnt)
+
+def rebuild_dataset(delete_old=False, update_mapping=False, verbose=True):
+    '''A convenient function for re-building dataset objects'''
+    for es_indexer in (BiogpsModelESIndexer, BiogpsDatasetReporterIndexer):
+        if delete_old:
+            es_indexer.delete_index_type(es_indexer.ES_INDEX_TYPE)
+            update_mapping = True   # if delete_old is True, update_mapping should be True anyway
+        es_indexer.build_index(update_mapping=update_mapping, verbose=verbose)
