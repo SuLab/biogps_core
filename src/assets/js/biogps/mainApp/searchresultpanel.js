87ab9f47fe1b2c29daef9fe4455ff50b5b0409c6
newgene <anewgene@yahoo.com>
2013-05-10 16:06:10 -0700

    Added species selector to search result page.

--- src/assets/js/biogps/mainApp/searchresultpanel.js
+++ src/assets/js/biogps/mainApp/searchresultpanel.js
@@ -41,6 +41,7 @@ biogps.GeneResultPage = function(config) {
 	this.start = this.resultPerPage*(this.currentPage-1) + 1;
 	this.end = Math.min(this.totalCount, this.start + this.resultPerPage -1);
 	this.totalPage = Math.ceil(this.totalCount/this.resultPerPage);
+    this.default_show_species = ['human', 'mouse', 'rat'];
 
 	biogps.GeneResultPage.superclass.constructor.call(this, {
 		border: false
@@ -112,8 +113,7 @@ biogps.GeneResultPage = function(config) {
 //				' '
 //			]
 	});
-//	this.on('render', this.renderGeneList, this);
-    this.on('render', this.renderGeneList2, this);
+    this.on('render', this.renderGeneList3, this);
 
 };
 
@@ -286,7 +286,165 @@ Ext.extend(biogps.GeneResultPage, Ext.Panel, {
 
 
         }
+    },
+
+    species_d:  {'9606':  'human',
+                 '10090': 'mouse',
+                 '10116': 'rat',
+                 '7227':  'fruitfly',
+                 '6239':  'nematode',
+                 '7955':  'zebrafish',
+                 '3702':  'thale-cress',
+                 '8364':  'frog',
+                 '9823':  'pig'
+                },
+    species_menu_labels:  { 'human': 'H. sapiens (human)',
+                            'mouse': 'M. musculus (mouse)',
+                            'rat': 'R. norvegicus (rat)',
+                            'fruitfly': 'D. melanogaster (fruitfly)',
+                            'nematode': 'C. elegans (nematode)',
+                            'zebrafish': 'D. rerio (zebrafish)',
+                            'thale-cress': 'A. thaliana (thale cress)',
+                            'frog': 'X. tropicalis (frog)',
+                            'pig': 'S. scrofa (pig)'
+                },
+
+    toggle_species: function(cb, evt){
+        $(cb).prev().attr('checked', !$(cb).prev().attr('checked'));
+        var species_selected = [];
+        $("input[name='generesult_species_list']:checked").each(function(){species_selected.push($(this).val());});
+        biogps.resultpage.show_species = species_selected;
+        biogps.resultpage.renderGeneList3();
+        return false;
+    },
+
+    renderGeneList3: function(){
+        this.show_species = this.show_species || this.default_show_species;
+
+        var species_d = this.species_d;
+        var species_menu_labels = this.species_menu_labels;
+
+        var species_list = [];
+        var species_counts = {}
+        var displayed_gene_cnt = 0;
+        var displayed_gene_list = [];
+        for (var i=0; i<this.geneList.length; i++){
+            var gene = this.geneList[i];
+            var taxid = gene.taxid.toString();
+            var species = species_d[taxid];
+            if (species_counts[species]){
+                species_counts[species].count += 1;
+            }
+            else{
+                species_counts[species] = {species: species,
+                                           label: species_menu_labels[species],
+                                           count: 1};
+            }
+            this.geneList[i].hide = false;
+            if (isArray(this.show_species)){
+                if(this.show_species.indexOf(species) == -1){
+                    this.geneList[i].hide = true;
+                }
+            }
+            if (!this.geneList[i].hide) {
+                displayed_gene_cnt+=1;
+                displayed_gene_list.push(this.geneList[i]);
+            }
+        }
+        for (species in species_menu_labels){
+            if (species_counts[species]){
+                if (isArray(this.show_species)){
+                    species_counts[species].checked = (this.show_species.indexOf(species) != -1)?"checked":"";
+                }
+                else {
+                    species_counts[species].checked = "checked";
+                }
+                species_list.push(species_counts[species]);
+            }
+        }
+
+        var tpl = new Ext.XTemplate(
+            '',
+            '<p class="generesult_header"><a class="generesult_goback" href="/#goto=welcome" title="Go back to query"></a>Your query ',
+            '<tpl if="this.useInlineQuery">',
+            '(<span class="generesult_query">"{query:ellipsis(50)}"</span>) ',
+            '</tpl>',
+            'returns {this.gene_count} record{this.plural}:</p>',
+
+            '<div id="generesult_species_selector">',
+            '<table class="generesult_species_selector" cellspacing="0">',
+            '<form>',
+            '<tbody>',
+            '<tpl for="this.species_list">',
+                '<tr><td><input type="checkbox" {checked} name="generesult_species_list" value="{species}" onclick="javascript:biogps.resultpage.toggle_species(this, event);"><a href="javascript:void(null);" onclick="javascript:biogps.resultpage.toggle_species(this, event);">{species}</a>&nbsp;&nbsp;</td><td>({count})&nbsp;&nbsp;&nbsp;&nbsp;</td><tr>',
+            '</tpl>',
+            '<tbody>',
+            '</form>',
+            '</table>',
+            '</div>',
+
+            '<table id="generesult_table" class="generesult_table" cellspacing="0">',
+            '<thead><tr><th scope="col">no.</th><tpl if="!this.useInlineQuery"><th scope="col">query</th></tpl><th scope="col">symbol</th><th scope="col">id</th><th scope="col" style="max-width:450px;">name</th><th scope="col">species</th></tr></thead><tbody>',
+            '<tpl for="this.gene_list">',
+                '<tpl if="!values.hide">',
+                    '<tr class="{[xindex % 2 === 0 ? "even" : "odd"]}"><th scope="row" onclick="biogps.renderGeneReport2(\'{id}\')">{#}</th><tpl if="!this.useInlineQuery"><td onclick="biogps.renderGeneReport2(\'{id}\')">{query}</td></tpl><td onclick="biogps.renderGeneReport2(\'{id}\')">{symbol}</td><td onclick="biogps.renderGeneReport2(\'{id}\')">{id}</td><td style="max-width:450px;" onclick="biogps.renderGeneReport2(\'{id}\')">{name}</td><td class="homologene_td" onclick="biogps.renderGeneReport2(\'{id}\')">{[this.fmtSpecies(values)]}</td></tr>',
+                '</tpl>',
+            '</tpl>',
+            '</tbody></table>',
+            '<div id="temp_for_notfound"></div>',
+            '<tpl if="values.notfound">',
+                '<p class="notfound_footer">Found no matches for {[values.notfound.length]} query term{[values.notfound.length>1?"s":""]}:<br />',
+                '<span class="notfound_hint">(Try wildcard query?)</span><br />',
+                '<tpl for="notfound">',
+                '<div class="notfound_term">{.}</div>',
+                '</tpl>',
+                '</p>',
+            '</tpl>',
+            {
+                compiled: true,
+                disableFormats: false,
+                useInlineQuery: (this.qtype=='keyword' || this.qtype=='interval'),
+                plural: displayed_gene_cnt > 1?'s':'',
+                species_list: species_list,
+                gene_list: displayed_gene_list,
+                gene_count: displayed_gene_cnt,
+                fmtSpecies: function(values){
+                    var tid = values.taxid.toString();
+                    var species = species_d[tid];
+                    out = String.format('<a title="{0}">{1}</a>', species_menu_labels[species], species);
+                    return out
+                }
+            }
+        );
+
+        if(this.rendered){
+            var html = tpl.apply(this);
+
+            var geneList = this.geneList;
+            this.body.update(html, false, function(){
+                var parent_el = Ext.get('result_panel');
+                var tbl = Ext.get('generesult_table');
+                tbl.anchorTo(parent_el, 'tl', [25, 50]);
+                Ext.get('generesult_species_selector').anchorTo(parent_el, 'tl', [650, 50]);
+                sorttable.makeSortable(tbl.dom);
+                biogps.Messenger.fireEvent('genelistrendered');
+                biogps.resultpage.genelistrendered = true;
+                //Goto genereport page directly if search result has just one gene
+                if (geneList.length==1){
+                    biogps.renderGeneReport2(geneList[0].id);
+                }
+                else{
+                    if (Ext.isIE) Ext.History.add('goto=searchresult');  //A fix for IE, otherwise it will switch back to welcome page after resultpanel is rendered (when search was submitted from welcome page)
+                }
+            },this);
+
+
+
+        }
     }
+
+
+
 
 });
 
