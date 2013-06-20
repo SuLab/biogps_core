cf83dc2b2fbf34ad753bd712bf35f1dec403dd77
newgene <anewgene@yahoo.com>
2013-06-19 18:14:54 -0700

    migrated apps.gene to v2 mygene.info api.

--- src/biogps/apps/gene/models.py
+++ src/biogps/apps/gene/models.py
@@ -1,14 +1,17 @@
 ''' Empty gene model used only for valid content type with favorites, etc.'''
 import copy
 
-from biogps.apps.boc import boc_svc as boc
+# from biogps.apps.boc import boc_svc as boc
+from biogps.apps.boe.views import MyGeneInfo
 from biogps.utils.remote_models import RemoteModel
 
 
 def get_gene_list(geneid_li):
     if geneid_li:
-        ds = boc.DataService()
-        gene_list = ds.querygenelist(geneid_li)
+        # ds = boc.DataService()
+        # gene_list = ds.querygenelist(geneid_li)
+        mg = MyGeneInfo()
+        gene_list = mg.querygenelist(geneid_li)
     else:
         gene_list = []
     return gene_list
@@ -16,7 +19,7 @@ def get_gene_list(geneid_li):
 
 class Gene(RemoteModel):
     short_name = 'gene'
-    _ds = boc.DataService()
+    _ds = MyGeneInfo()
     _default_fields = ['symbol', 'name', 'taxid', 'homologene']
 
     def _as_obj(self, dict):
@@ -30,15 +33,19 @@ class Gene(RemoteModel):
         return self
 
     def _get_obj_by_id(self, id):
-        _g = self._ds.getgene(id)
+        '''To support query like Gene.objects.get(id=1017)'''
+        _g = self._ds.get_gene(id, ','.join(self._default_fields))
         return self._as_obj(_g)
 
     def _get_obj_by_id_list(self, id_list):
-        gene_list = self._ds.getgenelist(id_list)
+        '''To support query like Gene.objects.filter(id__in=[1017, 1018])'''
+        # gene_list = self._ds.getgenelist(id_list)
+        gene_list = get_gene_list(id_list)
         return [copy.copy(self._as_obj(g)) for g in gene_list]
 
     def _count(self):
-        return self._ds.get_metadata()['TOTAL_GENE_DOC']
+        '''To support query like Gene.objects.count()'''
+        return self._ds.metadata['stats']['total_genes']
 
     def __unicode__(self):
         return u'"%s" (id: %s)' % (self.symbol, self.id)
