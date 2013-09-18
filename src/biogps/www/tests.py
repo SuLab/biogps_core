2658bcabdf3db25bae9f168b6a62d4895b230350
newgene <anewgene@yahoo.com>
2013-09-18 06:20:47 -0700

    updated tests for alt_layout and alt_dataset.

--- src/biogps/www/tests.py
+++ src/biogps/www/tests.py
@@ -22,10 +22,30 @@ def test_alt_layouts():
     for i in alt_layouts:
         res = c.get('/%s/' % i)
         eq_(res.status_code, 200)
+        assert res.content.find('biogps.alt_defaultlayout')!=-1, i
+
+    # Test arbitrary alt_layout
+    res = c.get('/?layout=83')
+    eq_(res.status_code, 200)
+    assert res.content.find('biogps.alt_defaultlayout = 83')!=-1
+    res = c.get('/?layout=foo')   #invalid value
+    eq_(res.status_code, 200)
+    assert res.content.find('biogps.alt_defaultlayout')==-1
 
     # Test missing/bad layout as well
     res = c.get('/missinglayout/')
     eq_(res.status_code, 404)
+
+
+def test_alt_dataset():
+    c = Client()
+    res = c.get('/?dataset=100')
+    eq_(res.status_code, 200)
+    assert res.content.find('biogps.alt_defaultdataset = 100')!=-1
+
+    res = c.get('/?dataset=foo')
+    eq_(res.status_code, 200)
+    assert res.content.find('biogps.alt_defaultdataset')==-1
 
 
 def test_flatpages():
