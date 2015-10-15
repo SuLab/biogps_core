98e0484852b5e1460c63975f40928cf38edf3fa7
Gustavo Andres Morero <gustavo@netladish.com>
2015-10-15 16:19:47 -0300

    removing src/assets folder (moved to src/biogps).

--- src/assets/js2/biogps/test/funcunit/biogps_test.js
+++
@@ -1,9 +0,0 @@
-module("biogps test", { 
-	setup: function(){
-        S.open("//biogps/biogps.html");
-	}
-});
-
-test("Copy Test", function(){
-	equals(S("h1").text(), "Welcome to JavaScriptMVC 3.0!","welcome text");
-});
