a1b54d85e9c2cb40e8215886ced9fef2f47e0328
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2020-10-01 18:50:39 +0000

    updated settings_private_example.py

--- src/biogps/biogps/settings_private_example.py
+++ src/biogps/biogps/settings_private_example.py
@@ -47,7 +47,27 @@ EMAIL_SUBJECT_PREFIX = '[BioGPS-Admin] '
 
 ######Third-party Django Apps specific settings#########
 
+#####################################################################
+#  django_ses or the AWS key does not work any more since 09/2018   #
+#  switch to use SES SMTP server on 09/10/2018                      #
+#####################################################################
+
 ## django_ses for sending email via SES
-EMAIL_BACKEND = 'django_ses.SESBackend'
-AWS_ACCESS_KEY_ID = '<aws_access_key_id>'
-AWS_SECRET_ACCESS_KEY = '<aws_secret_access_key>'
+# EMAIL_BACKEND = 'django_ses.SESBackend'
+# AWS_ACCESS_KEY_ID = '<aws_access_key_id>'
+# AWS_SECRET_ACCESS_KEY = '<aws_secret_access_key>'
+
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = '<smtp-server>'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 465
+
+## Google ReCaptcha v3 settings
+# https://developers.google.com/recaptcha/docs/v3
+# https://developers.google.com/recaptcha/docs/verify
+RECAPTCHA_PRIVATE_KEY = '<private_key>'
+RECAPTCHA_PUBLIC_KEY = '<public_key>'
+RECAPTCHA_API = 'https://www.google.com/recaptcha/api/siteverify'
+RECAPTCHA_REQUIRED_SCORE = 0.85
